5e0ae3b85e7c893219144614d3f9ba11cb19cfce
Cherpak <125959803+SeregaCherpak@users.noreply.github.com>
2023-06-08 16:25:34 +0600

    Update listJS.js

--- LR2/src/main/webapp/listJS.js
+++ LR2/src/main/webapp/listJS.js
@@ -12,8 +12,29 @@ xhr.onreadystatechange = function() {
             for (value of Object.values(element)) {
                 str += "<td>" + value + "</td>";
             }
-            table_body.innerHTML += str;
-            table_body.innerHTML += "</tr>";
+            str+="<td><button type='button' id='delBtn" + n + "' name='delBtn' class='delBtn btn btn-danger rounded'>Удалить</button></td>";
+                str+="<td><button type='button' id='editBtn" + n + "' name='editBtn' class='editBtn btn btn-primary rounded'>Редактировать</button></td>";
+                table_body.innerHTML += str;
+                table_body.innerHTML += "</tr>";
+                n++;
+            }
+            let delButtons = document.getElementsByName("delBtn");
+            for (let button of delButtons) {
+                let id = button.parentElement.previousSibling.innerText;
+                button.onclick = function() {
+                    let xhr1 = new XMLHttpRequest();
+                    xhr1.open("POST", "delete", true);
+                    xhr1.send(id);
+                    setTimeout(location.reload(), 1000);
+                }
+                let editButtons = document.getElementsByName("editBtn");
+                for (let button of editButtons) {
+                    button.onclick = function() {
+                        let id = button.parentElement.previousSibling.previousSibling.innerText;
+                        setTimeout(location.href = "edit?id=" + id, 1000);
+                    }
+                }
+            }
         }
     }
 }

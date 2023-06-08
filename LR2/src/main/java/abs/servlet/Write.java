ca8084cc7675a7df2b34f80437d7cab7221d8f6d
Cherpak <125959803+SeregaCherpak@users.noreply.github.com>
2023-06-08 16:38:30 +0600

    Update Write.java

--- LR2/src/main/java/abs/servlet/Write.java
+++ LR2/src/main/java/abs/servlet/Write.java
@@ -16,30 +16,47 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mysql.cj.jdbc.Driver; // Класс для установления подключения к БД
+
 @WebServlet("/main")
 public class Write extends HttpServlet {
+    private Connection connection;
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletContext sc = getServletContext();
-        sc.getRequestDispatcher("/jsp/main.jsp").forward(request, response);
+        sc.getRequestDispatcher("/jsp/main.jsp").forward(req, resp);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver(); 
+            DriverManager.registerDriver(driver); 
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:8080/servlet_war","Admin","0000"); 
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String data = request.getReader().readLine();
+        Car car = gson.fromJson(data, Car.class);
+        String query = String.format("insert into Car(mark, model, price, year, engine_capacity) values ('%s','%s','%s','%s','%s')",
+                series.getmark(),
+                series.getmodel(),
+                series.getprice(),
+                series.getyear(),
+                series.getengine_capacity());
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query); // Выполнение SQL инструкции
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String pathStr = "C:\\Users\\Сергей\\Desktop\\Институт\\4\\LR2\\src\\main\\webapp\\car.json";
-        Path path = Paths.get(pathStr);
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        ArrayList<Car> Car = new ArrayList<>();
-        String jsonArrayString = "1";
-        if (!Files.exists(path)) {
-            Files.createFile(path);
-        }
-        jsonArrayString = Files.readString(path);
-        if (jsonArrayString != "") {
-            Type list = new TypeToken<ArrayList<Car>>() {}.getType();
-            Car = gson.fromJson(jsonArrayString, list);
-        }
-        String data = request.getReader().readLine();
-        Car.add(gson.fromJson(data, Car.class));
-        Files.write(path, gson.toJson(Car).getBytes());
-    }
 }

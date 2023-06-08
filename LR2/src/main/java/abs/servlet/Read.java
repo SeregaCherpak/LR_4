32e37c645ae25868c79e6cea42a73f95fa46ef41
Cherpak <125959803+SeregaCherpak@users.noreply.github.com>
2023-06-08 16:34:36 +0600

    Update Read.java

--- LR2/src/main/java/abs/servlet/Read.java
+++ LR2/src/main/java/abs/servlet/Read.java
@@ -10,24 +10,56 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.mysql.cj.jdbc.Driver; // Класс для установления подключения к БД
 
 @WebServlet("/list")
 public class Read extends HttpServlet {
+    private Connection connection;
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ServletContext sc = getServletContext();
-        sc.getRequestDispatcher("/jsp/list.jsp").forward(request, response);
+        sc.getRequestDispatcher("/jsp/list.jsp").forward(req, resp);
     }
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String pathStr = "C:\\Users\\Сергей\\Desktop\\Институт\\4\\LR2\\src\\main\\webapp\\car.json";
-        Path path = Paths.get(pathStr);
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver(); 
+            DriverManager.registerDriver(driver); 
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:8080/series_database", "Admin", "0000"); 
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         response.setContentType("application/json");
-        String jsonArrayString = "";
-        if (Files.exists(path)) {
-            jsonArrayString = Files.readString(path);
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String query = String.format("select * from Car");
+        ArrayList<Car> car = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(query); 
+            while (resultSet.next()) {
+                Car car = new Series(resultSet.getString("mark"),
+                        resultSet.getString("genre"),
+                        resultSet.getString("model"),
+                        resultSet.getString("price"),
+                        resultSet.getString("year"),
+                        resultSet.getString("engine_capacity"),
+                car.add(car);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         PrintWriter out = response.getWriter();
+        String jsonArrayString = "";
+        if (series.size() != 0) {
+            jsonArrayString = gson.toJson(car);
+        }
         out.print(jsonArrayString);
         out.close();
     }
